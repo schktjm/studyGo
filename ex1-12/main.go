8b08999bf26a1ff984fe1df55e521d9edf58280d
schktjm <si2.o.mjt@gmail.com>
2019-03-03 16:35:22 +0900

    bug? / ex1-12

---
+++ ex1-12/main.go
@@ -0,0 +1,68 @@
+// Lissajous generates GIF animations of random Lissajous figures.
+// write Lissajous figures on localhost:8000
+// and if the URL like http://localhost:8000/?circles=20 , number of oscillator revolutions make 20
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
+)
+
+// Q. なぜqueryが何もない時何も表示されないの?
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		num := 5
+		if q := r.URL.Query(); q != nil {
+			s := q.Get("cycles")
+			num, _ = strconv.Atoi(s) //errorつぶしてます
+		}
+		lissajous(w, num)
+	}
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func lissajous(out io.Writer, num int) {
+	const (
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	var cycles = float64(num)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
+
+//!-main
